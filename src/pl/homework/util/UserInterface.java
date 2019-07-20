985df2343d9dc11ac2e1d106ce8398e4d3151292
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-20 12:51:33 +0200

    Homework 6.4  scanner.close()

--- src/pl/homework/util/UserInterface.java
+++ src/pl/homework/util/UserInterface.java
@@ -2,13 +2,14 @@ package pl.homework.util;
 
 import pl.homework.lib.Product;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class UserInterface {
     public Product getProduct() {
         Product product = new Product();
         Scanner scanner = new Scanner(System.in);
-
+        scanner.useLocale(Locale.US);
         System.out.println("Podaj dane produktu :");
         System.out.println(" - nazwa: ");
         product.setName(scanner.nextLine());
@@ -19,6 +20,7 @@ public class UserInterface {
         scanner.nextLine();
         System.out.println(" - kategoria: ");
         product.setCategory(scanner.nextLine());
+        scanner.close();
         return product;
     }
 }
